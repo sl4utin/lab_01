237385d90d108074e3c76eaba90d089c60d04c1e
SEM <sl4utinm@yandex.ru>
2022-10-26 13:35:53 +0500

    NOD EVKLID V2

---
+++ main.py
@@ -0,0 +1,8 @@
+a=int(input("Введите первое число: "))
+b=int(input("Введите второе число: "))
+while(a!=0 and b!=0):
+    if(a>b):
+        a=a%b
+    else:
+        b=b%a
+print(f"Полученный НОД: {a+b}")
